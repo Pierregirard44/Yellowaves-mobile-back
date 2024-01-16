c0c05157c17f185aea104aebb411899f30da554f
quentinkerzerho <quentin.kerzerho@gmail.com>
2024-01-16 10:31:49 +0100

    refactorisation de records

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -242,133 +242,133 @@ func getRecordByID(w http.ResponseWriter, r *http.Request) {
 
 func getAllSpotInfo(w http.ResponseWriter, r *http.Request) {
 	// Simule la récupération des enregistrements depuis votre source de données (base de données, fichier, etc.)
-	records := []SurfRecord{
-		{
-			ID: "rec5aF9TjMjBicXCK",
-			Fields: RecordFields{
-				SurfBreak:        []string{"Reef Break"},
-				DifficultyLevel:  4,
-				Destination:      "Pipeline",
-				Geocode:          "🔵 eyJpIjoiUGlwZWxpbmUsIE9haHUsIEhhd2FpaSIsIm8iOnsic3RhdHVzIjoiT0siLCJmb3JtYXR0ZWRBZGRyZXNzIjoiRWh1a2FpIEJlYWNoIFBhcmssIEhhbGVpd2EsIEhJIDk2NzEyLCBVbml...",
-				Influencers:      []string{"recD1zp1pQYc8O7l2", "rec1ptbRPxhS8rRun"},
-				MagicSeaweedLink: "https://magicseaweed.com/Pipeline-Backdoor-Surf-Report/616/",
-				Photos: []Photo{
-					{
-						ID:       "attf6yu03NAtCuv5L",
-						URL:      "https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg",
-						Filename: "thomas-ashlock-64485-unsplash.jpg",
-						Size:     688397,
-						Type:     "image/jpeg",
-						Thumbnails: Thumbnails{
-							Small: Small{
-								URL:    "https://dl.airtable.com/yfKxR9ZQqiT7drKxpjdF_small_thomas-ashlock-64485-unsplash.jpg",
-								Width:  52,
-								Height: 36,
-							},
-							Large: Large{
-								URL:    "https://dl.airtable.com/cFfMuU8NQjaEskeC3B2h_large_thomas-ashlock-64485-unsplash.jpg",
-								Width:  744,
-								Height: 512,
-							},
-							Full: Full{
-								URL:    "https://dl.airtable.com/psynuQNmSvOTe3BWa0Fw_full_thomas-ashlock-64485-unsplash.jpg",
-								Width:  2233,
-								Height: 1536,
-							},
-						},
-					},
-				},
-				PeakSurfSeasonBegins:    "2018-07-22",
-				DestinationStateCountry: "Oahu, Hawaii",
-				PeakSurfSeasonEnds:      "2018-08-31",
-				Address:                 "Pipeline, Oahu, Hawaii",
-			},
-			CreatedTime: "2018-05-31T00:16:16.000Z",
-		},
-		{
-			ID: "recT98Z2El7YYwmc4",
+	// records := []SurfRecord{
+	// 	{
+	// 		ID: "rec5aF9TjMjBicXCK",
+	// 		Fields: RecordFields{
+	// 			SurfBreak:        []string{"Reef Break"},
+	// 			DifficultyLevel:  4,
+	// 			Destination:      "Pipeline",
+	// 			Geocode:          "🔵 eyJpIjoiUGlwZWxpbmUsIE9haHUsIEhhd2FpaSIsIm8iOnsic3RhdHVzIjoiT0siLCJmb3JtYXR0ZWRBZGRyZXNzIjoiRWh1a2FpIEJlYWNoIFBhcmssIEhhbGVpd2EsIEhJIDk2NzEyLCBVbml...",
+	// 			Influencers:      []string{"recD1zp1pQYc8O7l2", "rec1ptbRPxhS8rRun"},
+	// 			MagicSeaweedLink: "https://magicseaweed.com/Pipeline-Backdoor-Surf-Report/616/",
+	// 			Photos: []Photo{
+	// 				{
+	// 					ID:       "attf6yu03NAtCuv5L",
+	// 					URL:      "https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg",
+	// 					Filename: "thomas-ashlock-64485-unsplash.jpg",
+	// 					Size:     688397,
+	// 					Type:     "image/jpeg",
+	// 					Thumbnails: Thumbnails{
+	// 						Small: Small{
+	// 							URL:    "https://dl.airtable.com/yfKxR9ZQqiT7drKxpjdF_small_thomas-ashlock-64485-unsplash.jpg",
+	// 							Width:  52,
+	// 							Height: 36,
+	// 						},
+	// 						Large: Large{
+	// 							URL:    "https://dl.airtable.com/cFfMuU8NQjaEskeC3B2h_large_thomas-ashlock-64485-unsplash.jpg",
+	// 							Width:  744,
+	// 							Height: 512,
+	// 						},
+	// 						Full: Full{
+	// 							URL:    "https://dl.airtable.com/psynuQNmSvOTe3BWa0Fw_full_thomas-ashlock-64485-unsplash.jpg",
+	// 							Width:  2233,
+	// 							Height: 1536,
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 			PeakSurfSeasonBegins:    "2018-07-22",
+	// 			DestinationStateCountry: "Oahu, Hawaii",
+	// 			PeakSurfSeasonEnds:      "2018-08-31",
+	// 			Address:                 "Pipeline, Oahu, Hawaii",
+	// 		},
+	// 		CreatedTime: "2018-05-31T00:16:16.000Z",
+	// 	},
+	// 	{
+	// 		ID: "recT98Z2El7YYwmc4",
 
-			Fields: RecordFields{
-				SurfBreak:        []string{"Point Break"},
-				DifficultyLevel:  5,
-				Destination:      "Skeleton Bay",
-				Geocode:          "🔵 eyJpIjoiU2tlbGV0b24gQmF5LCBOYW1pYmlhIiwibyI6eyJzdGF0dXMiOiJPSyIsImZvcm1hdHRlZEFkZHJlc3MiOiJOYW1pYmlhIiwibGF0IjotMjUuOTE0NDkxOSwibG5nIjoxNC45MDY4NTk...",
-				Influencers:      []string{"recD1zp1pQYc8O7l2", "rec1ptbRPxhS8rRun"},
-				MagicSeaweedLink: "https://magicseaweed.com/Skeleton-Bay-Surf-Report/4591/",
-				Photos: []Photo{
-					{
-						ID:       "attKinKKZvgdS5A5U",
-						URL:      "https://dl.airtable.com/YzqA020RRLaTyAZAta9g_brandon-compagne-308937-unsplash.jpg",
-						Filename: "brandon-compagne-308937-unsplash.jpg",
-						Size:     1494974,
-						Type:     "image/jpeg",
-						Thumbnails: Thumbnails{
-							Small: Small{
-								URL:    "https://dl.airtable.com/Y8970kyrQHWfL6AMkxZQ_small_brandon-compagne-308937-unsplash.jpg",
-								Width:  54,
-								Height: 36,
-							},
-							Large: Large{
-								URL:    "https://dl.airtable.com/dkQKXoUnTGiofIvg5TJR_large_brandon-compagne-308937-unsplash.jpg",
-								Width:  768,
-								Height: 512,
-							},
-							Full: Full{
-								URL:    "https://dl.airtable.com/pexuxaQ6D2VV61pyhUwn_full_brandon-compagne-308937-unsplash.jpg",
-								Width:  3000,
-								Height: 2000,
-							},
-						},
-					},
-				},
-				PeakSurfSeasonBegins:    "2018-09-01",
-				DestinationStateCountry: "Namibia",
-				PeakSurfSeasonEnds:      "2018-11-30",
-				Address:                 "Skeleton Bay, Namibia",
-			},
-		},
-		{
-			ID: "recH2ennHFNOtB1Wt",
-			Fields: RecordFields{
-				SurfBreak:        []string{"Point Break"},
-				DifficultyLevel:  4,
-				Destination:      "Superbank",
-				Geocode:          "🔵 eyJpIjoiU3VwZXJiYW5rLCBHb2xkIENvYXN0LCBBdXN0cmFsaWEiLCJvIjp7InN0YXR1cyI6Ik9LIiwiZm9ybWF0dGVkQWRkcmVzcyI6IlNuYXBwZXIgUm9ja3MgUmQsIENvb2xhbmdhdHRhIFF...",
-				Influencers:      []string{"recSkJ4HuvzAUBrdd"},
-				MagicSeaweedLink: "https://magicseaweed.com/Surfers-Paradise-Gold-Coast-Surf-Report/1012/",
-				Photos: []Photo{
-					{
-						ID:       "attmtbEOAQteRjz2p",
-						URL:      "https://dl.airtable.com/I4E4xZeQbO2g814udQDm_jeremy-bishop-80371-unsplash.jpg",
-						Filename: "jeremy-bishop-80371-unsplash.jpg",
-						Size:     1524876,
-						Type:     "image/jpeg",
-						Thumbnails: Thumbnails{
-							Small: Small{
-								URL:    "https://dl.airtable.com/IWP9RPvvSM2pX1sHeigV_small_jeremy-bishop-80371-unsplash.jpg",
-								Width:  48,
-								Height: 36,
-							},
-							Large: Large{
-								URL:    "https://dl.airtable.com/aBnINo8qQqDvER2f2wGg_large_jeremy-bishop-80371-unsplash.jpg",
-								Width:  683,
-								Height: 512,
-							},
-							Full: Full{
-								URL:    "https://dl.airtable.com/eZxgLD8hRI2Y27J39LNl_full_jeremy-bishop-80371-unsplash.jpg",
-								Width:  3000,
-								Height: 2250,
-							},
-						},
-					},
-				},
-				PeakSurfSeasonBegins:    "2018-11-28",
-				DestinationStateCountry: "Gold Coast, Australia",
-				PeakSurfSeasonEnds:      "2019-02-18",
-			},
-			CreatedTime: "2018-05-31T00:16:16.000Z",
-		},
-	}
+	// 		Fields: RecordFields{
+	// 			SurfBreak:        []string{"Point Break"},
+	// 			DifficultyLevel:  5,
+	// 			Destination:      "Skeleton Bay",
+	// 			Geocode:          "🔵 eyJpIjoiU2tlbGV0b24gQmF5LCBOYW1pYmlhIiwibyI6eyJzdGF0dXMiOiJPSyIsImZvcm1hdHRlZEFkZHJlc3MiOiJOYW1pYmlhIiwibGF0IjotMjUuOTE0NDkxOSwibG5nIjoxNC45MDY4NTk...",
+	// 			Influencers:      []string{"recD1zp1pQYc8O7l2", "rec1ptbRPxhS8rRun"},
+	// 			MagicSeaweedLink: "https://magicseaweed.com/Skeleton-Bay-Surf-Report/4591/",
+	// 			Photos: []Photo{
+	// 				{
+	// 					ID:       "attKinKKZvgdS5A5U",
+	// 					URL:      "https://dl.airtable.com/YzqA020RRLaTyAZAta9g_brandon-compagne-308937-unsplash.jpg",
+	// 					Filename: "brandon-compagne-308937-unsplash.jpg",
+	// 					Size:     1494974,
+	// 					Type:     "image/jpeg",
+	// 					Thumbnails: Thumbnails{
+	// 						Small: Small{
+	// 							URL:    "https://dl.airtable.com/Y8970kyrQHWfL6AMkxZQ_small_brandon-compagne-308937-unsplash.jpg",
+	// 							Width:  54,
+	// 							Height: 36,
+	// 						},
+	// 						Large: Large{
+	// 							URL:    "https://dl.airtable.com/dkQKXoUnTGiofIvg5TJR_large_brandon-compagne-308937-unsplash.jpg",
+	// 							Width:  768,
+	// 							Height: 512,
+	// 						},
+	// 						Full: Full{
+	// 							URL:    "https://dl.airtable.com/pexuxaQ6D2VV61pyhUwn_full_brandon-compagne-308937-unsplash.jpg",
+	// 							Width:  3000,
+	// 							Height: 2000,
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 			PeakSurfSeasonBegins:    "2018-09-01",
+	// 			DestinationStateCountry: "Namibia",
+	// 			PeakSurfSeasonEnds:      "2018-11-30",
+	// 			Address:                 "Skeleton Bay, Namibia",
+	// 		},
+	// 	},
+	// 	{
+	// 		ID: "recH2ennHFNOtB1Wt",
+	// 		Fields: RecordFields{
+	// 			SurfBreak:        []string{"Point Break"},
+	// 			DifficultyLevel:  4,
+	// 			Destination:      "Superbank",
+	// 			Geocode:          "🔵 eyJpIjoiU3VwZXJiYW5rLCBHb2xkIENvYXN0LCBBdXN0cmFsaWEiLCJvIjp7InN0YXR1cyI6Ik9LIiwiZm9ybWF0dGVkQWRkcmVzcyI6IlNuYXBwZXIgUm9ja3MgUmQsIENvb2xhbmdhdHRhIFF...",
+	// 			Influencers:      []string{"recSkJ4HuvzAUBrdd"},
+	// 			MagicSeaweedLink: "https://magicseaweed.com/Surfers-Paradise-Gold-Coast-Surf-Report/1012/",
+	// 			Photos: []Photo{
+	// 				{
+	// 					ID:       "attmtbEOAQteRjz2p",
+	// 					URL:      "https://dl.airtable.com/I4E4xZeQbO2g814udQDm_jeremy-bishop-80371-unsplash.jpg",
+	// 					Filename: "jeremy-bishop-80371-unsplash.jpg",
+	// 					Size:     1524876,
+	// 					Type:     "image/jpeg",
+	// 					Thumbnails: Thumbnails{
+	// 						Small: Small{
+	// 							URL:    "https://dl.airtable.com/IWP9RPvvSM2pX1sHeigV_small_jeremy-bishop-80371-unsplash.jpg",
+	// 							Width:  48,
+	// 							Height: 36,
+	// 						},
+	// 						Large: Large{
+	// 							URL:    "https://dl.airtable.com/aBnINo8qQqDvER2f2wGg_large_jeremy-bishop-80371-unsplash.jpg",
+	// 							Width:  683,
+	// 							Height: 512,
+	// 						},
+	// 						Full: Full{
+	// 							URL:    "https://dl.airtable.com/eZxgLD8hRI2Y27J39LNl_full_jeremy-bishop-80371-unsplash.jpg",
+	// 							Width:  3000,
+	// 							Height: 2250,
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 			PeakSurfSeasonBegins:    "2018-11-28",
+	// 			DestinationStateCountry: "Gold Coast, Australia",
+	// 			PeakSurfSeasonEnds:      "2019-02-18",
+	// 		},
+	// 		CreatedTime: "2018-05-31T00:16:16.000Z",
+	// 	},
+	//}
 
 	// Convertit les enregistrements en JSON
 	jsonData, err := json.Marshal(records)
