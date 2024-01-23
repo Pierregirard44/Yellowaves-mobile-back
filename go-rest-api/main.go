3cf6e79e82c21d4107b324dda00fd860f8908f03
MarieCoulay <marie.coulay@gmail.com>
2024-01-23 14:36:40 +0100

    changement fonction main

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+
+	//"log"
 	"net/http"
 
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
@@ -232,11 +236,32 @@ func getAllSpotInfo(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonData)
 }
 
-func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/spots", getAllSpotInfo).Methods("GET")
-	router.HandleFunc("/spots/{id}", getRecordByID).Methods("GET")
+// func main() {
+// 	router := mux.NewRouter().StrictSlash(true)
+// 	router.HandleFunc("/", homeLink)
+// 	router.HandleFunc("/spots", getAllSpotInfo).Methods("GET")
+// 	router.HandleFunc("/spots/{id}", getRecordByID).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+//		log.Fatal(http.ListenAndServe(":8080", router))
+//	}
+func main() {
+	// Connexion à la base de données MySQL à distance
+	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/yellowaves")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Exemple de requête SELECT
+	var spots struct {
+		Destination string `db:"destination"`
+		Difficulty  int    `db:"difficulty"`
+	}
+	err = db.QueryRow("SELECT * FROM yellowavestable").Scan(&spots.Destination, &spots.Difficulty)
+	if err != nil {
+		panic(err)
+	}
+
+	// Utilisez les données de l'utilisateur ici
+	fmt.Println("Destination:", spots.Destination, "Difficulté:", spots.Difficulty)
 }
