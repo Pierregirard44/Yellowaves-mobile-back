da20f6041bd7957e5ee805d099b03ff01d0da1db
quentinkerzerho <quentin.kerzerho@gmail.com>
2024-01-23 15:25:11 +0100

    ça affiche le json nice(brice)

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -4,237 +4,48 @@ import (
 	"encoding/json"
 	"fmt"
 
-	//"log"
-	"net/http"
+	"log"
+	//"net/http"
 
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	//"github.com/gorilla/mux"
 )
 
-// Structure pour représenter un enregistrement
-type SurfRecord struct {
-	ID          string       `json:"id"`
-	Fields      RecordFields `json:"fields"`
-	CreatedTime string       `json:"createdTime"`
-}
+// func homeLink(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Bienvenue sur le back de Yellowaves !")
+// }
 
-// Structure pour représenter les champs d'un enregistrement
-type RecordFields struct {
-	SurfBreak               []string `json:"Surf Break"`
-	DifficultyLevel         int      `json:"Difficulty Level"`
-	Destination             string   `json:"Destination"`
-	Geocode                 string   `json:"Geocode"`
-	Influencers             []string `json:"Influencers"`
-	MagicSeaweedLink        string   `json:"Magic Seaweed Link"`
-	Photos                  []Photo  `json:"Photos"`
-	PeakSurfSeasonBegins    string   `json:"Peak Surf Season Begins"`
-	DestinationStateCountry string   `json:"Destination State/Country"`
-	PeakSurfSeasonEnds      string   `json:"Peak Surf Season Ends"`
-	Address                 string   `json:"Address"`
-}
+// func getRecordByID(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	recordID := vars["id"]
 
-// Structure pour représenter les photos
-type Photo struct {
-	ID         string     `json:"id"`
-	URL        string     `json:"url"`
-	Filename   string     `json:"filename"`
-	Size       int        `json:"size"`
-	Type       string     `json:"type"`
-	Thumbnails Thumbnails `json:"thumbnails"`
-}
+// 	for _, record := range records {
+// 		if record.ID == recordID {
+// 			json.NewEncoder(w).Encode(record)
+// 			return
+// 		}
+// 	}
 
-// Structure pour représenter les miniatures des photos
-type Thumbnails struct {
-	Small Small `json:"small"`
-	Large Large `json:"large"`
-	Full  Full  `json:"full"`
-}
+// 	http.Error(w, "Record not found", http.StatusNotFound)
+// }
 
-// Structure pour représenter une miniature
-type Small struct {
-	URL    string `json:"url"`
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-}
+// func getAllSpotInfo(w http.ResponseWriter, r *http.Request) {
 
-// Structure pour représenter une grande image
-type Large struct {
-	URL    string `json:"url"`
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-}
+// 	// Convertit les enregistrements en JSON
+// 	jsonData, err := json.Marshal(records)
+// 	if err != nil {
+// 		http.Error(w, "Erreur lors de la conversion en JSON", http.StatusInternalServerError)
+// 		return
+// 	}
 
-// Structure pour représenter une image en taille réelle
-type Full struct {
-	URL    string `json:"url"`
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-}
+// 	// Définit le type de contenu de la réponse HTTP comme application/json
+// 	w.Header().Set("Content-Type", "application/json")
 
-var records = []SurfRecord{
-	{
-		ID: "rec5aF9TjMjBicXCK",
-		Fields: RecordFields{
-			SurfBreak:        []string{"Reef Break"},
-			DifficultyLevel:  4,
-			Destination:      "Pipeline",
-			Geocode:          "🔵 eyJpIjoiUGlwZWxpbmUsIE9haHUsIEhhd2FpaSIsIm8iOnsic3RhdHVzIjoiT0siLCJmb3JtYXR0ZWRBZGRyZXNzIjoiRWh1a2FpIEJlYWNoIFBhcmssIEhhbGVpd2EsIEhJIDk2NzEyLCBVbml...",
-			Influencers:      []string{"recD1zp1pQYc8O7l2", "rec1ptbRPxhS8rRun"},
-			MagicSeaweedLink: "https://magicseaweed.com/Pipeline-Backdoor-Surf-Report/616/",
-			Photos: []Photo{
-				{
-					ID:       "attf6yu03NAtCuv5L",
-					URL:      "https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg",
-					Filename: "thomas-ashlock-64485-unsplash.jpg",
-					Size:     688397,
-					Type:     "image/jpeg",
-					Thumbnails: Thumbnails{
-						Small: Small{
-							URL:    "https://dl.airtable.com/yfKxR9ZQqiT7drKxpjdF_small_thomas-ashlock-64485-unsplash.jpg",
-							Width:  52,
-							Height: 36,
-						},
-						Large: Large{
-							URL:    "https://dl.airtable.com/cFfMuU8NQjaEskeC3B2h_large_thomas-ashlock-64485-unsplash.jpg",
-							Width:  744,
-							Height: 512,
-						},
-						Full: Full{
-							URL:    "https://dl.airtable.com/psynuQNmSvOTe3BWa0Fw_full_thomas-ashlock-64485-unsplash.jpg",
-							Width:  2233,
-							Height: 1536,
-						},
-					},
-				},
-			},
-			PeakSurfSeasonBegins:    "2018-07-22",
-			DestinationStateCountry: "Oahu, Hawaii",
-			PeakSurfSeasonEnds:      "2018-08-31",
-			Address:                 "Pipeline, Oahu, Hawaii",
-		},
-		CreatedTime: "2018-05-31T00:16:16.000Z",
-	},
-	{
-		ID: "recT98Z2El7YYwmc4",
-
-		Fields: RecordFields{
-			SurfBreak:        []string{"Point Break"},
-			DifficultyLevel:  5,
-			Destination:      "Skeleton Bay",
-			Geocode:          "🔵 eyJpIjoiU2tlbGV0b24gQmF5LCBOYW1pYmlhIiwibyI6eyJzdGF0dXMiOiJPSyIsImZvcm1hdHRlZEFkZHJlc3MiOiJOYW1pYmlhIiwibGF0IjotMjUuOTE0NDkxOSwibG5nIjoxNC45MDY4NTk...",
-			Influencers:      []string{"recD1zp1pQYc8O7l2", "rec1ptbRPxhS8rRun"},
-			MagicSeaweedLink: "https://magicseaweed.com/Skeleton-Bay-Surf-Report/4591/",
-			Photos: []Photo{
-				{
-					ID:       "attKinKKZvgdS5A5U",
-					URL:      "https://dl.airtable.com/YzqA020RRLaTyAZAta9g_brandon-compagne-308937-unsplash.jpg",
-					Filename: "brandon-compagne-308937-unsplash.jpg",
-					Size:     1494974,
-					Type:     "image/jpeg",
-					Thumbnails: Thumbnails{
-						Small: Small{
-							URL:    "https://dl.airtable.com/Y8970kyrQHWfL6AMkxZQ_small_brandon-compagne-308937-unsplash.jpg",
-							Width:  54,
-							Height: 36,
-						},
-						Large: Large{
-							URL:    "https://dl.airtable.com/dkQKXoUnTGiofIvg5TJR_large_brandon-compagne-308937-unsplash.jpg",
-							Width:  768,
-							Height: 512,
-						},
-						Full: Full{
-							URL:    "https://dl.airtable.com/pexuxaQ6D2VV61pyhUwn_full_brandon-compagne-308937-unsplash.jpg",
-							Width:  3000,
-							Height: 2000,
-						},
-					},
-				},
-			},
-			PeakSurfSeasonBegins:    "2018-09-01",
-			DestinationStateCountry: "Namibia",
-			PeakSurfSeasonEnds:      "2018-11-30",
-			Address:                 "Skeleton Bay, Namibia",
-		},
-	},
-	{
-		ID: "recH2ennHFNOtB1Wt",
-		Fields: RecordFields{
-			SurfBreak:        []string{"Point Break"},
-			DifficultyLevel:  4,
-			Destination:      "Superbank",
-			Geocode:          "🔵 eyJpIjoiU3VwZXJiYW5rLCBHb2xkIENvYXN0LCBBdXN0cmFsaWEiLCJvIjp7InN0YXR1cyI6Ik9LIiwiZm9ybWF0dGVkQWRkcmVzcyI6IlNuYXBwZXIgUm9ja3MgUmQsIENvb2xhbmdhdHRhIFF...",
-			Influencers:      []string{"recSkJ4HuvzAUBrdd"},
-			MagicSeaweedLink: "https://magicseaweed.com/Surfers-Paradise-Gold-Coast-Surf-Report/1012/",
-			Photos: []Photo{
-				{
-					ID:       "attmtbEOAQteRjz2p",
-					URL:      "https://dl.airtable.com/I4E4xZeQbO2g814udQDm_jeremy-bishop-80371-unsplash.jpg",
-					Filename: "jeremy-bishop-80371-unsplash.jpg",
-					Size:     1524876,
-					Type:     "image/jpeg",
-					Thumbnails: Thumbnails{
-						Small: Small{
-							URL:    "https://dl.airtable.com/IWP9RPvvSM2pX1sHeigV_small_jeremy-bishop-80371-unsplash.jpg",
-							Width:  48,
-							Height: 36,
-						},
-						Large: Large{
-							URL:    "https://dl.airtable.com/aBnINo8qQqDvER2f2wGg_large_jeremy-bishop-80371-unsplash.jpg",
-							Width:  683,
-							Height: 512,
-						},
-						Full: Full{
-							URL:    "https://dl.airtable.com/eZxgLD8hRI2Y27J39LNl_full_jeremy-bishop-80371-unsplash.jpg",
-							Width:  3000,
-							Height: 2250,
-						},
-					},
-				},
-			},
-			PeakSurfSeasonBegins:    "2018-11-28",
-			DestinationStateCountry: "Gold Coast, Australia",
-			PeakSurfSeasonEnds:      "2019-02-18",
-			Address:                 "Superbank, Gold Coast, Australia",
-		},
-		CreatedTime: "2018-05-31T00:16:16.000Z",
-	},
-} // Assurez-vous de remplir cette slice avec vos données
-
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bienvenue sur le back de Yellowaves !")
-}
-
-func getRecordByID(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	recordID := vars["id"]
-
-	for _, record := range records {
-		if record.ID == recordID {
-			json.NewEncoder(w).Encode(record)
-			return
-		}
-	}
-
-	http.Error(w, "Record not found", http.StatusNotFound)
-}
-
-func getAllSpotInfo(w http.ResponseWriter, r *http.Request) {
-
-	// Convertit les enregistrements en JSON
-	jsonData, err := json.Marshal(records)
-	if err != nil {
-		http.Error(w, "Erreur lors de la conversion en JSON", http.StatusInternalServerError)
-		return
-	}
-
-	// Définit le type de contenu de la réponse HTTP comme application/json
-	w.Header().Set("Content-Type", "application/json")
-
-	// Envoie la réponse JSON
-	w.Write(jsonData)
-}
+// 	// Envoie la réponse JSON
+// 	w.Write(jsonData)
+// }
 
 // func main() {
 // 	router := mux.NewRouter().StrictSlash(true)
@@ -253,7 +64,7 @@ func main() {
 	defer db.Close()
 
 	//Exemple de requête SELECT
-	var spots struct {
+	type Spot struct {
 		Destination             string `db:"destination"`
 		Difficulty              int    `db:"difficulty"`
 		DestinationStateCountry string `db:"DestinationStateCountry"`
@@ -262,11 +73,52 @@ func main() {
 		PeakSurfSeasonBegins    string `db:"PeakSurfSeasonBegins"`
 		PeakSurfSeasonEnds      string `db:"PeakSurfSeasonEnds"`
 	}
-	err = db.QueryRow("SELECT * FROM yellowavestable").Scan(&spots.Destination, &spots.DestinationStateCountry, &spots.Difficulty, &spots.SurfBreak, &spots.Photos, &spots.PeakSurfSeasonBegins, &spots.PeakSurfSeasonEnds)
+	// err = db.QueryRow("SELECT * FROM yellowavestable").Scan(&spots.Destination, &spots.DestinationStateCountry, &spots.Difficulty, &spots.SurfBreak, &spots.Photos, &spots.PeakSurfSeasonBegins, &spots.PeakSurfSeasonEnds)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// // Utilisez les données de l'utilisateur ici
+	// fmt.Println("Destination:", spots.Destination, "Difficulté:", spots.Difficulty, "DestinationStateCountry:", spots.DestinationStateCountry, "SurfBreak:", spots.SurfBreak, "photos:", spots.Photos, "PeakSurfSeasonBegins:", spots.PeakSurfSeasonBegins, "PeakSurfSeasonEnds:", spots.PeakSurfSeasonEnds)
+	rows, err := db.Query("SELECT * FROM yellowavestable")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Créer une slice pour stocker les résultats
+	var spotsList []Spot
+
+	// Parcourir les résultats
+	for rows.Next() {
+		var spots Spot
+		err := rows.Scan(
+			&spots.Destination,
+			&spots.DestinationStateCountry,
+			&spots.Difficulty,
+			&spots.SurfBreak,
+			&spots.Photos,
+			&spots.PeakSurfSeasonBegins,
+			&spots.PeakSurfSeasonEnds,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		spotsList = append(spotsList, spots)
 	}
 
-	// Utilisez les données de l'utilisateur ici
-	fmt.Println("Destination:", spots.Destination, "Difficulté:", spots.Difficulty, "DestinationStateCountry:", spots.DestinationStateCountry, "SurfBreak:", spots.SurfBreak, "photos:", spots.Photos, "PeakSurfSeasonBegins:", spots.PeakSurfSeasonBegins, "PeakSurfSeasonEnds:", spots.PeakSurfSeasonEnds)
+	// Gérer les erreurs éventuelles de parcours des résultats
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Encoder en JSON
+	jsonData, err := json.MarshalIndent(spotsList, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Afficher le JSON
+	fmt.Println(string(jsonData))
+
 }
